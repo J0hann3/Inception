8f08457273132ab39261a2a856e4f9ee2303040d
J0hann3 <jvigny@student.42.fr>
2024-02-23 13:00:02 -0500

    [ADD] opti dockerfile + [ADD] check if mariadb is arlready configure before setting up

--- srcs/requirements/mariadb/tools/mariadb_config.sh
+++ srcs/requirements/mariadb/tools/mariadb_config.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-service mariadb start
+service mariadb start > /dev/null
 
-echo "name :${MYSQL_DATABASE} | root_password :${MYSQL_ROOT_PASSWORD} | user :${MYSQL_USER} | user_password :${MYSQL_PASSWORD}"
-# RUN mysql_secure_installation
+# check if mariadb is already set up
+database=$(echo "SHOW DATABASES LIKE '${MYSQL_DATABASE}';" | mysql 2> /dev/null)
 
-# set root password
-mysqladmin --user=root password ${MSQYL_ROOT_PASSWORD}
-# RUN mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$MSQYL_ROOT_PASSWORD{}') WHERE User='root';"
+if [[ -z $database ]];
+then
 
-# Remove anonyomous users
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "DELETE FROM mysql.user WHERE User='';"
+	# RUN mysql_secure_installation
 
-# Remove remote root login
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	# set root password
+	mysqladmin --user=root password ${MYSQL_ROOT_PASSWORD}
+	# RUN mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD{}') WHERE User='root';"
 
-# Remove test database 
-# mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "DROP DATABASE test;" # error
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+	# Remove anonyomous users
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "DELETE FROM mysql.user WHERE User='';"
 
-# Reload privilege tables
-# mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
+	# Remove remote root login
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 
-# add a second users
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost';"
+	# Remove test database 
+	# mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "DROP DATABASE test;" # error
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
 
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE};"
+	# Reload privilege tables
+	# mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
 
-# Reload privilege tables
-mysql -u root -p${MSQYL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
+	# Add a second users
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+	GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost';"
+
+	# Create database wordpress
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE};"
+
+	# Reload privilege tables
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
+
+else
+
+	echo "Database already created !"
+
+fi
+
+exec "$@"
