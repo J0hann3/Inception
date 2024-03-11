a7d3e0d54e13b8b03d051544bde4c79fe9618d9b
J0hann3 <jvigny@student.42.fr>
2024-03-11 15:16:53 -0400

    [ADD] SSL connection

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-service nginx start
-
 rm /etc/nginx/sites-enabled/*
 
-service nginx stop
+mkdir /etc/nginx/ssl; \
+openssl req -newkey rsa:4096 -nodes -keyout /etc/nginx/ssl/wordpress.key \
+	-x509 -days 365 -sha256 \
+	-out /etc/nginx/ssl/wordpress.crt \
+	-subj "/C=FR/ST=Ile-de-France/L=Paris/O=42/";
 
 nginx -t
+
+service nginx start
+service nginx stop
 
 exec "$@"
